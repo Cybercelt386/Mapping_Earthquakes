dca126c254a8380665cd16b071929a1fb84d5299
Alekzander Holmes <Holmes.alekzander@gmail.com>
2022-12-09 15:18:58 -0800

    start_up

--- main/js/logic.js
+++ main/js/logic.js
@@ -1,11 +1,13 @@
 //we create the map object with options
-let map = L.map('mapid').setView([40.7,-94.5],4)
-.addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v12'));
+let map = L.map('mapid').setView([40.7,-94.5],4);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/1/1/0?access_token=pk.eyJ1IjoiY3liZXJjZWx0Mzg2IiwiYSI6ImNsYmNwODlxYzFveDIzbnBrd3F1cG5mcGcifQ.nM81HgZGU5LjaAmSJo_Y9A", {
-attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+{attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    tileSize:512,
     maxZoom: 18,
+    zoomOffset: -1,
+    id:"mapbox/steets-v11",
     accessToken: API_KEY
 });
 
