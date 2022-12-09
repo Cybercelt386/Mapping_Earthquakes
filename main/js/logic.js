eb60baecac54d442ef471208058410e20d8365c3
Alekzander Holmes <Holmes.alekzander@gmail.com>
2022-12-09 15:44:03 -0800

    fini

--- main/js/logic.js
+++ main/js/logic.js
@@ -3,14 +3,23 @@ let map = L.map('mapid').setView([40.7,-94.5],4);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+//let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
 {attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     tileSize:512,
     maxZoom: 18,
     zoomOffset: -1,
     id:"mapbox/steets-v11",
     accessToken: API_KEY
-});
+}); 
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+//  Add a marker to the map for Los Angeles, California.
+let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+L.circle([34.0522, -118.2437], {
+    radius: 300,
+    color: "black",
+    fillColor:"#ffffa1"
+ }).addTo(map);
