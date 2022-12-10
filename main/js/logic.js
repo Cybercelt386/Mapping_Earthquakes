1d3b31a2f18f0e20a19fdf5b064bdcdd06d73904
Alekzander Holmes <Holmes.alekzander@gmail.com>
2022-12-10 13:55:40 -0800

    compleate

--- main/js/logic.js
+++ main/js/logic.js
@@ -1,5 +1,6 @@
 //we create the map object with options
-let map = L.map('mapid').setView([40.7,-94.5],4);
+//let map = L.map('mapid').setView([40.7,-94.5],4);
+let map = L.map('mapid').setView([36.1733, -120.1794], 7);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -75,3 +76,18 @@ cityData.forEach(function(city) {
   .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString + "</h3>")
   .addTo(map);
 });
+
+
+// Coordinates for each point to be used in the polyline.
+let line = [
+  [33.9416, -118.4085],
+  [37.6214, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
+];
+
+// Create a polyline using the line coordinates and make the line yellow.
+L.polyline(line, {
+  color: "red"
+}).addTo(map);
+
